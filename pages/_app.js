eeedd85e9a4015a983e4eb684b36ea9c7e1dfa59
KhaizenNomazen <aghastyaxx@gmail.com>
2026-03-10 07:48:22 +0700

    Add Cloudflare Turnstile script to MyApp

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,14 @@
 import "../styles/globals.css";
+import Script from "next/script";
 
 export default function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Script
+        src="https://challenges.cloudflare.com/turnstile/v0/api.js"
+        strategy="beforeInteractive"
+      />
+      <Component {...pageProps} />
+    </>
+  );
 }
