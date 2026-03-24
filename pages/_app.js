64f3bbe6137eb5bbb20c20d805c230f9a4141de8
KhaizenNomazen <aghastyaxx@gmail.com>
2026-03-24 17:36:03 +0700

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -6,6 +6,11 @@ export default function MyApp({ Component, pageProps }) {
     <>
       <Head>
         <meta name="google-site-verification" content="_TodwBix_GYhoYVAnOn-h5r_xxK17EWlp6wiUiLVQ0k" />
+        <link rel="icon" type="image/x-icon" href="/favicon.ico" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+        <meta name="theme-color" content="#2dd4bf" />
       </Head>
       <Component {...pageProps} />
     </>
